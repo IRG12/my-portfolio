453b342dc1e267dd6bd02cea65c9d0e1de01b3a3
Isaiah G <isaiahrg12@gmail.com>
2023-03-23 12:29:56 -0500

    Update
    I'm applying to jobs again.

--- pages/about.jsx
+++ pages/about.jsx
@@ -5,20 +5,16 @@ const AboutPage = () => {
     <>
       <h2 className={styles.lineH}>
         {" "}
-        My name is Isaiah Gutierrez and I am a Front End Dev at the age of 31. I
-        like to be active with going to the gym, walks, hiking or, helping out
-        at my Church. I've been working labor jobs for the past ten years. Two
-        years ago, I decided to learn programming and I thought "What do you got
-        to lose. If this doesn't work out, at least you can check it off your
-        list and move on." Low and behold I was slowly grasping it. Though in
-        the beginning of 2021 I hit a wall trying to complete my first tutorial.
-        I did research and tried to follow but my code kept throwing errors. I
-        didn't know what to do. I knew I needed teacher or mentor. I came a
-        cross Clever Programmer, did some research, and decided to join their
-        October boot camp cohort. I learned the fundamentals of programing and
-        graduated as a "Front-End Software Engineer". I have complete over 20+
-        tutorials since then and more to come. I hope you have a good day and
-        best wishes to you and yours!
+        My name is Isaiah Gutierrez, a Front End Developer with over two years
+        of experience. I graduated from a coding boot camp as a Front End
+        Developer, and I am currently enrolled in a more advanced coding
+        academy. In addition to being a student, I also serve as a tutor in the
+        academy, helping others develop their coding skills. I am passionate
+        about building responsive and accessible web applications using modern
+        technologies and tools. I enjoy keeping up-to-date with the latest
+        trends and best practices in the industry. With strong problem-solving
+        skills and attention to detail, I take pride in delivering high-quality
+        user experience thats meets the needs of clients and users.
       </h2>
     </>
   );
