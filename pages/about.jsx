61753be04b2076db4147c1a2fdc3592ebc6bb10f
Isaiah G <isaiahrg12@gmail.com>
2023-03-24 09:16:46 -0500

    Update
    Updated 'about section' bio.

--- pages/about.jsx
+++ pages/about.jsx
@@ -5,16 +5,17 @@ const AboutPage = () => {
     <>
       <h2 className={styles.lineH}>
         {" "}
-        My name is Isaiah Gutierrez, a Front End Developer with over two years
-        of experience. I graduated from a coding boot camp as a Front End
-        Developer, and I am currently enrolled in a more advanced coding
-        academy. In addition to being a student, I also serve as a tutor in the
-        academy, helping others develop their coding skills. I am passionate
-        about building responsive and accessible web applications using modern
-        technologies and tools. I enjoy keeping up-to-date with the latest
-        trends and best practices in the industry. With strong problem-solving
-        skills and attention to detail, I take pride in delivering high-quality
-        user experience thats meets the needs of clients and users.
+        Hello! Thank you for stopping by! I am a Front-End Developer and
+        Designer. Front-End skills: - HTML - CSS - Javascript - React.js -
+        Tailwind - Node.js - Redux toolkit Design tools: WordPress - Figma -
+        Canva - WordPress - Photoshop In addition to being a student, I also
+        volunteer as a tutor in the academy, helping others develop their coding
+        skills. I am passionate about building responsive and accessible web
+        applications using modern technologies and tools. I enjoy keeping
+        up-to-date with the latest trends and best practices in the industry.
+        With strong problem-solving skills and attention to detail, I take pride
+        in delivering high-quality user experience thats meets the needs of
+        clients and users.
       </h2>
     </>
   );
