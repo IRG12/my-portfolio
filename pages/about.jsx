b656de81cec505842379c3b10b5cd718ec87acb2
Isaiah G <isaiahrg12@gmail.com>
2023-06-21 14:34:49 -0500

    Sending resume to client

--- pages/about.jsx
+++ pages/about.jsx
@@ -5,13 +5,12 @@ const AboutPage = () => {
     <>
       <h2 className={styles.lineH}>
         {" "}
-        Hello! Thank you for stopping by! I am a Front-End Developer and
-        Designer. Front-End skills: - HTML - CSS - Javascript - React.js -
-        Tailwind - Node.js - Redux toolkit Design tools: WordPress - Figma -
-        Canva - WordPress - Photoshop In addition to being a student, I also
-        volunteer as a tutor in the academy, helping others develop their coding
-        skills. I am passionate about building responsive and accessible web
-        applications using modern technologies and tools. I enjoy keeping
+        I am a Front-End Developer and Designer. <br> </br> Front-End skills: -
+        HTML - CSS - Javascript - React.js - Tailwind - Node.js - Redux toolkit{" "}
+        <br></br> Design tools: WordPress - Figma - Canva - Photoshop <br></br>I
+        also volunteer as a tutor in the academy, helping others develop their
+        coding skills. I am passionate about building responsive and accessible
+        web applications using modern technologies and tools. I enjoy keeping
         up-to-date with the latest trends and best practices in the industry.
         With strong problem-solving skills and attention to detail, I take pride
         in delivering high-quality user experience thats meets the needs of
