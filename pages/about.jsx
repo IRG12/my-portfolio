5dbbf7c9739facab69d0ebcde3b1b95e174293ea
Isaiah G <isaiahrg12@gmail.com>
2023-06-22 14:24:15 -0500

    Trying again

--- pages/about.jsx
+++ pages/about.jsx
@@ -5,16 +5,16 @@ const AboutPage = () => {
     <>
       <h2 className={styles.lineH}>
         {" "}
-        I am a Front-End Developer and Designer. <br> </br> Front-End skills: -
-        HTML - CSS - Javascript - React.js - Tailwind - Node.js - Redux toolkit{" "}
-        <br></br> Design tools: WordPress - Figma - Canva - Photoshop <br></br>I
-        also volunteer as a tutor in the academy, helping others develop their
-        coding skills. I am passionate about building responsive and accessible
-        web applications using modern technologies and tools. I enjoy keeping
-        up-to-date with the latest trends and best practices in the industry.
-        With strong problem-solving skills and attention to detail, I take pride
-        in delivering high-quality user experience thats meets the needs of
-        clients and users.
+        I am a Front-End Developer and Designer. Front-End skills: - HTML - CSS
+        - Javascript - React.js - Tailwind - Node.js - Redux toolkit Design
+        tools: WordPress - Figma - Canva - Photoshop also volunteer as a tutor
+        in the academy, helping others develop their coding skills. I am
+        passionate about building responsive and accessible web applications
+        using modern technologies and tools. I enjoy keeping up-to-date with the
+        latest trends and best practices in the industry. With strong
+        problem-solving skills and attention to detail, I take pride in
+        delivering high-quality user experience thats meets the needs of clients
+        and users.
       </h2>
     </>
   );
